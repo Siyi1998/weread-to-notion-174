bced6e5210f6f51e5eb94a0c72e9afc6d6d7e29b
Siyi1998 <51387740+Siyi1998@users.noreply.github.com>
2023-09-01 22:23:25 +0800

    Update weread.py

--- weread.py
+++ weread.py
@@ -128,14 +128,14 @@ def get_quote(content):
 
 def get_callout(content, style, colorStyle, reviewId):
     # 根据不同的划线样式设置不同的emoji 直线type=0 背景颜色是1 波浪线是2
-    emoji = "🌟"
+    emoji = "🌿"
     if style == 0:
-        emoji = "💡"
+        emoji = "🌻"
     elif style == 1:
-        emoji = "⭐"
+        emoji = "🍒"
     # 如果reviewId不是空说明是笔记
     if reviewId != None:
-        emoji = "✍️"
+        emoji = "🌈"
     color = "default"
     # 根据划线颜色设置文字的颜色
     if colorStyle == 1:
